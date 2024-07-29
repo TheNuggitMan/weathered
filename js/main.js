7f32157e9d98d3569c4bae7d9493c6a1ba4159a2
Lee <thenuggitman@gmail.com>
2024-07-29 14:57:26 +0100

    Added secure http request

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const celcius = '℃';
 const fahrenheit = '℉';
 /* 
 Make the GeoCall
-http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
 Make the weather call
 https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
